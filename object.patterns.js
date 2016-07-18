cc1869a7e6af40dd1b55b716782117ec559ddd5f
Ryan O'Hern <ryanohern@gmail.com>
2016-07-18 15:18:18 -0400

    added objects and functions

---
+++ object.patterns.js
@@ -0,0 +1,56 @@
+var bike = (function() {
+
+    // private
+    var wheels = 2;
+    var bellSound = "ring, ring";
+
+    return {
+        model: "monocog",
+
+        wheelCount: function() {
+            console.log( wheels );
+        },
+
+        ringBell: function() {
+            console.log( bellSound );
+        },
+
+        setBell: function( newSound ) {
+            bellSound = newSound;
+        }
+    };
+})();
+
+// console.log(bike.wheels);
+// bike.wheelCount();
+// bike.ringBell();
+// bike.setBell("dingaling");
+// bike.ringBell();
+
+
+///////////////////////////////////////////
+
+var kermit = (function(){
+	// private
+	var color = "green";
+	var myFavoriteColor = "blue";
+
+	// public
+	return {
+		myColor: 				function (){
+									console.log(color);
+								},
+		guessMyFavoriteColor: 	function (guess){
+									if (guess === myFavoriteColor){
+										console.log("Correct, my favorite color is " + myFavoriteColor);	
+									} else {
+										console.log("Nope. Try again.");
+									}
+								}
+	};
+})();
+
+
+console.log(kermit.myFavoriteColor); // will return undefined - no cheating!
+kermit.guessMyFavoriteColor(""); // Guess Kermit's favorite color here
+
--- objects.js
+++ objects.js
@@ -16,4 +16,134 @@ var fooWithProperties =  {
 
 // create an object that represents a bike
 // and allows us to ring a bell and define properties
+/*
+var bike = {
+	make: 			"Honda",
+	wheels: 		2, 
+	hasBell: 		false,
+	hasHandlebars: 	true,
+	year: 			"2004",
+	color: 			"green",
+	ringBell: 		function (){
+						if (this.hasBell){
+							console.log("ring, ring");
+						} else {
+							console.log("I need a bell.");
+						}
+					},
+	installBell:    function () {
+						this.hasBell = true;
+					} 		
+};
 
+bike.ringBell();
+bike.installBell();
+bike.ringBell();
+
+THIS //////////////////////////////////
+function ringGlobalBell (){
+	console.log(this.bellSound);
+}
+
+var bike1 = {
+	bellSound: "ring, ring",
+	ringBell: function(){
+		console.log(this.bellSound);
+	}
+};
+
+var bike2 = {
+	bellSound: "ding, ding",
+	ringBell: function(){
+		console.log(this.bellSound);
+	}
+};
+
+var bellSound = "dingaling";
+// ringGlobalBell();
+bike1.ringBell();
+bike2.ringBell();
+
+// USING CALL() /////////////////
+function ringGlobalBell ( bike ) {
+	console.log( bike + ": " + this.bellSound);
+}
+
+var bike1 = {
+	bellSound: "ring, ring"
+};
+
+
+var bike2 = {
+	bellSound: "ding, ding"
+};
+
+var bellSound = "dingaling";
+
+// call() - will accept two arguments
+ringGlobalBell.call( this, "global");
+// call as bike1
+ringGlobalBell.call( bike1, "bike1");
+// call as bike2
+ringGlobalBell.call( bike2, "bike2");
+
+
+// USING APPLY() //////////////////////////////////
+// you can pass an array of arguments
+ringGlobalBell.apply( this, ["global"]);
+ringGlobalBell.apply( bike1, ["bike1"]);
+ringGlobalBell.apply( bike2, ["bike2"]);
+
+// USING BIND() ///////////////////////////////////
+var ringBike1Bell = ringGlobalBell.bind(bike1);
+ringBike1Bell("bike1");
+
+var ringBike2Bell = ringGlobalBell.bind(bike2);
+ringBike2Bell("bike2");
+
+// once bound, it can't change
+bike2.ringBell = ringBike1Bell;
+bike2.ringBell("bike2");
+
+
+
+// CONSTRUCTORS
+
+function Bike(model) {
+	this.model = 		model;
+	this.logModel = 	function(){
+							console.log(this.model);
+						};
+}
+
+var bike1 = new Bike("Defy");
+var mountainBike = new Bike("Monocog");
+
+console.log(bike1 instanceof Bike);
+console.log(mountainBike instanceof Bike);
+
+console.log(bike1.model);
+bike1.logModel();
+
+// adding to a prototype
+Bike.prototype.wheels = 2;
+console.log(bike1.wheels);
+console.log(mountainBike.wheels);
+*/
+
+//ADDING MULTIPLE METHODS/PROPERTIES TO PROTOTYPE
+
+function Bike ( model ) {
+	this.model = model;
+}
+
+Bike.prototype = {
+	constructor: Bike, //tells you what the constructor is
+	wheels: 	2,
+	logModel: 	function (){
+					console.log( this.model );
+				}
+};
+
+var bike1 = new Bike("defy");
+console.log(bike1.constructor);
